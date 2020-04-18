2ab8920c46033e50cf2f6f303d225292427d9180
Munro-L <lindsaymunromail@gmail.com>
2020-04-17 22:48:28 -0400

    Made 2 docker containers. One for the bot, and one that runs a Jupyter notebook for training models. Both can be spawned from the docker_up.sh script for ease of use.

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM tensorflow/tensorflow:nightly-py3
-RUN pip3 install jupyter jupyterthemes pandas pyyaml h5py tensorflow_datasets
-RUN jt -t onedork
--- docker_up.sh
+++ docker_up.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-docker build -t discordnlptrainer:latest .
-docker run -i -t --network=host --workdir=/DiscordNLP -v $(pwd):/DiscordNLP discordnlptrainer:latest jupyter notebook --allow-root --port=8889
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    echo "Execute 'docker_up.sh bot' to run the bot, or 'docker_up.sh trainer' to run the trainer"
+    exit
+fi
+
+if ! [[ $(which docker) && $(docker --version) ]]; then
+    echo "Docker is not installed. Make sure it is installed, and your user is in the Docker group."
+    exit
+fi
+
+if [ "$1" == "bot" ]; then
+    docker build -t discordnlp:bot - < Dockerfile_bot
+    docker run -i -t --network=host --workdir=/DiscordNLP -v $(pwd):/DiscordNLP discordnlp:bot python3 DiscordNLP.py
+
+elif [ "$1" == "trainer" ]; then
+    docker build -t discordnlp:trainer - < Dockerfile_trainer
+    docker run -i -t --network=host --workdir=/DiscordNLP -v $(pwd):/DiscordNLP discordnlp:trainer jupyter notebook --allow-root --port=8889
+fi
